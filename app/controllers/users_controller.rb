57976e6835da75fa9c68e1a79e5646ba4944b741
raksha bhalavi <raksha.b@goldeneagle.ai>
2025-01-24 21:16:41 +0530

    change password and edit profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,11 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!  # Ensures the user is logged in
+  before_action :authenticate_user!
 
-  # Edit action to show the user's profile
+  # Edit profile (name, email, timezone)
   def edit
     @user = current_user
   end
 
-  # Update action to handle form submission
   def update
     @user = current_user
     if @user.update(user_params)
@@ -16,9 +15,29 @@ class UsersController < ApplicationController
     end
   end
 
+  # Change password page
+  def change_password
+    @user = current_user
+  end
+
+  def update_password
+		@user = current_user
+		if @user.update_with_password(password_params)
+			redirect_to profile_path, notice: 'Password updated successfully.'
+		else
+			render :change_password
+		end
+	end
+
   private
 
+  # Permit user parameters for profile update
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)  # Add other fields here
+    params.require(:user).permit(:name, :email, :timezone)
+  end
+
+  # Permit password parameters for password update
+  def password_params
+    params.require(:user).permit(:current_password, :password, :password_confirmation)
   end
 end
