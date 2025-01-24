f7567315f3d460941a8375025664be92dbc1a9a4
raksha bhalavi <raksha.b@goldeneagle.ai>
2025-01-24 19:55:48 +0530

    edit profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  # Override the after_sign_in_path_for method to redirect users to their profile page
+  def after_sign_in_path_for(resource)
+    profile_path  # This will redirect to the profile page after login
+  end
 end
