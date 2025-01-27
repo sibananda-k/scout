08c083df846f52d3567db5b108c46d582a809381
raksha bhalavi <raksha.b@goldeneagle.ai>
2025-01-27 17:40:28 +0530

    resolved conflict

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_01_24_105452) do
+ActiveRecord::Schema[7.0].define(version: 2025_01_24_142804) do
   create_table "organisations", force: :cascade do |t|
     t.string "organisation_name"
     t.datetime "created_at", null: false
@@ -26,6 +26,8 @@ ActiveRecord::Schema[7.0].define(version: 2025_01_24_105452) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "organisation_id"
+    t.string "timezone"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["organisation_id"], name: "index_users_on_organisation_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
